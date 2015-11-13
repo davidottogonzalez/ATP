089fde4e676d535a201569dced322929800f4546
David Gonzalez <David@Davids-MacBook-Pro.local>
2015-11-13 11:54:49 -0500

    Refactor AppDB class to add, update, and delete correctly -Decouple methods from attribute specifics

--- atp_classes/AppDB.py
+++ atp_classes/AppDB.py
@@ -40,28 +40,28 @@ class AppDB:
     def get_document_by_field(self, collection, field, val):
         return self.db[collection].find_one({field: val})
 
-    def update_collection(self, collection, attribute):
+    def update_collection(self, collection, obj):
+        update_id = obj["_id"]
+        del obj["_id"]
+
         self.db[collection].update_one(
-            {"_id": ObjectId(attribute["_id"])},
+            {"_id": ObjectId(update_id)},
             {
-                "$set": {
-                    "logical_expression": attribute["logical_expression"],
-                    "name": attribute["name"]
-                }
+                "$set": obj
             }
         )
 
-        return self.db[collection].find_one({"_id": ObjectId(attribute["_id"])})
+        return self.db[collection].find_one({"_id": ObjectId(update_id)})
 
-    def add_to_collection(self, collection, attribute):
-        del attribute['id']
-        attr_id = self.db[collection].insert_one(attribute).inserted_id
+    def add_to_collection(self, collection, obj):
+        del obj['id']
+        new_id = self.db[collection].insert_one(obj).inserted_id
 
-        return self.db[collection].find_one({"_id": attr_id})
+        return self.db[collection].find_one({"_id": new_id})
 
-    def remove_from_collection(self, collection, attribute):
+    def remove_from_collection(self, collection, obj):
         result = self.db[collection].remove(
-            {"_id": ObjectId(attribute["_id"])},
+            {"_id": ObjectId(obj["_id"])},
             {
              "justOne": True
            }
