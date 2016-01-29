b57b992867c21632bc2a982e6d5513f287e57c9c
David Gonzalez <David@Davids-MacBook-Pro.local>
2016-01-29 11:39:53 -0500

    Add query definition and specs to the cross-tab and segment results (both dialog and exported Excel) - Add row to results table in both partials - Add a function to convert logical expression object to a string - Add property to segment controller to use convert to string function

--- static/services/LogicalExpressionService.js
+++ static/services/LogicalExpressionService.js
@@ -190,6 +190,34 @@ angular.module('ServicesModule', ['ngFileSaver']).factory('LogicalExpressionServ
         return operandDrops[operandDrops.length - 1];
     };
 
+    var convertToString = function(expression) {
+        var expressionString = '(';
+
+        if(expression.operand1 != null && typeof expression.operand1 == 'object') {
+            if(typeof expression.operand1.operand1 != 'undefined') {
+                expressionString += convertToString(expression.operand1);
+            }
+            else if(typeof expression.operand1.name != 'undefined') {
+                expressionString += expression.operand1.name + ' '
+            }
+        }
+
+        if(expression.operator != null && typeof expression.operator == 'object') {
+            expressionString += ' ' +  expression.operator.name + ' '
+        }
+
+        if(expression.operand2 != null && typeof expression.operand2 == 'object') {
+            if(typeof expression.operand2.operand1 != 'undefined') {
+                expressionString += convertToString(expression.operand2);
+            }
+            else if(typeof expression.operand2.name != 'undefined') {
+                expressionString += expression.operand2.name + ' '
+            }
+        }
+
+        return expressionString + ')';
+    }
+
     return {
         createNew: function(obj) {
             return new LogicalExpressionInstance(obj);
@@ -197,6 +225,7 @@ angular.module('ServicesModule', ['ngFileSaver']).factory('LogicalExpressionServ
         isValidDrop: isValidDrop,
         isExpressionNotEmpty: isExpressionNotEmpty,
         getFirstEmptyOperandDrop: getFirstEmptyOperandDrop,
-        getLastOperandDrop: getLastOperandDrop
+        getLastOperandDrop: getLastOperandDrop,
+        convertToString: convertToString
     };
 });
